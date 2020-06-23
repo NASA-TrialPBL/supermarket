ef7a8a54b8563d1c5f7f067dfb8230b441fe3a0c
DevGosugi <goki.sugimura89@gmail.com>
2020-06-24 05:27:05 +0900

    Initial Commit

---
+++ src/test/java/com/example/supermarket/SupermarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.supermarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupermarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
